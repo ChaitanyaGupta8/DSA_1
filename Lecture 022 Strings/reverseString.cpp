38b9fddc78fa59ea582f8c168d8711983492676d
ChaitanyaGupta8 <guptachaitanya0810@gmail.com>
2024-11-08 22:37:32 +0530

    Create reverseString.cpp

---
+++ Lecture 022 Strings/reverseString.cpp
@@ -0,0 +1,13 @@
+//leetcode 344
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int st = 0 , end = s.size() - 1;
+
+        while(st < end){
+            swap(s[st++] , s[end--]);
+
+        }
+
+    }
+};
